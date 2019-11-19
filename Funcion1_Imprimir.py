1e66ed63bd05d3bafa0def61e0952a8c2f043406
ANGRODHER <angelrodriguezh98@gmail.com>
2019-11-18 22:02:17 -0400

    temporal

--- Funcion1_Imprimir.py
+++ Funcion1_Imprimir.py
@@ -2,12 +2,14 @@
 def imprimir(tablero:[int])->"void":				# Procedimiento el tablero separándolo por pisos
 	
 	print("")
-	print("   1  2  3  4  5  6  7  8")
+	#print("   1  2  3  4  5  6  7  8")
+	print("   0  1  2  3  4  5  6  7")
 	for i in range(0,8):
-		print(letra1(i),tablero[i],letra2(i))
+		print(i,tablero[i],i)
+	print("   0  1  2  3  4  5  6  7")	
+	#print("   1  2  3  4  5  6  7  8")	
 		
-	print("   8  7  6  5  4  3  2  1")	
-def letra1(numero:int)->str:
+"""def letra1(numero:int)->str:
 	if numero==0:
 		var="A"
 	elif numero==1:
@@ -24,24 +26,7 @@ def letra1(numero:int)->str:
 		var="G"
 	elif numero==7:
 		var="H"
-	return var	
+	return var	"""
 
-def letra2(numero:int)->str:
-	if numero==0:
-		var="H"
-	elif numero==1:
-		var="G"
-	elif numero==2:
-		var="F"
-	elif numero==3:
-		var="E"
-	elif numero==4:
-		var="D"
-	elif numero==5:
-		var="C"
-	elif numero==6:
-		var="B"
-	elif numero==7:
-		var="A"
-	return var	
+
 				
